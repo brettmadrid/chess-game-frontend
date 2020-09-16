44168377ecdbe3c41a4068fcfcee631d69645db5
brettmadrid <brettmadrid9@gmail.com>
2020-09-15 22:28:06 -0700

    completed chesspiece object

--- src/chess/model/chesspiece.js
+++ src/chess/model/chesspiece.js
@@ -1,18 +1,39 @@
 class ChessPiece {
-  constructor(name, id, isAttacked, color) {
+  constructor(name, isAttacked, color, id) {
     this.name = name // string
-    this.id = id // string
     this.isAttacked = isAttacked // boolean
-    this.color = color // string: 'white' || 'black'
+    this.color = color // string
+    this.id = id // string
   }
 
-  setSquare() {
-    // assign this piece to a specific square
-    // this.square = square
+  setSquare(newSquare) {
+    // set the square this piece is sitting top of.
+    // on any given piece (on the board), there will always be a piece on top of it.
+    // console.log(newSquare)
+    if (newSquare === undefined) {
+      this.squareThisPieceIsOn = newSquare
+      return
+    }
+
+    if (this.squareThisPieceIsOn === undefined) {
+      this.squareThisPieceIsOn = newSquare
+      newSquare.setPiece(this)
+    }
+
+    const isNewSquareDifferent =
+      this.squareThisPieceIsOn.x != newSquare.x ||
+      this.squareThisPieceIsOn.y != newSquare.y
+
+    if (isNewSquareDifferent) {
+      // console.log("set")
+      this.squareThisPieceIsOn = newSquare
+      newSquare.setPiece(this)
+    }
   }
 
   getSquare() {
-    // get the current square this piece is on
-    // undefined if this piece is not on the board
+    return this.squareThisPieceIsOn
   }
 }
+
+export default ChessPiece
