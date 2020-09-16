a357eff930973abe7e3ac2156e625ff0d30fff5d
brettmadrid <brettmadrid9@gmail.com>
2020-09-15 19:57:42 -0700

    spec out react piece component

---
+++ src/chess/ui/piece.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { image } from 'react-konva'
+
+const Piece = props => {
+  /**
+   * imageURL for this piece
+   * color
+   * id
+   * - color of player
+   * - whether or not it's your turn
+   * - if is this player's turn, color of this player is the same color as this piece,
+   *   then make the piece draggable/moveable
+   */
+  return <Image drabbable />
+}
+
+export default Piece
